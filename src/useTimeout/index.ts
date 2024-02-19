525618f46b873a2cd908cda34cce9274ba1c0e73
kwooshung <kwooshung@qq.com>
2024-02-19 15:48:55 +0800

    💩 optimization(lib): Optimize Timer, call the corresponding function from Lomind

--- src/useTimeout/index.ts
+++ src/useTimeout/index.ts
@@ -1,30 +1,20 @@
-import { useEffect, useRef, useCallback } from 'react';
+import { useEffect, useRef } from 'react';
+import { createTimeout } from 'lomind';
 
-/**
- * @zh 用于管理定时器的 hook
- * @en Hook for managing timers
- * @param {() => void} callback 回调函数
- * @param {number | null} delay 延迟时间
- * @returns {() => void} 清除定时器的函数
- */
 const useTimeout = (callback: () => void, delay: number | null): (() => void) => {
-  const timeoutId = useRef<number | null>(null);
-
-  const clear = useCallback(() => {
-    if (timeoutId.current !== null) {
-      clearTimeout(timeoutId.current);
-      timeoutId.current = null;
-    }
-  }, []);
+  const clear = useRef<(() => void) | null>(null);
 
   useEffect(() => {
-    if (delay !== null) {
-      timeoutId.current = window.setTimeout(callback, delay);
-    }
-    return clear;
-  }, [delay, callback, clear]);
+    delay && (clear.current = createTimeout(callback, delay));
 
-  return clear;
+    return () => {
+      clear.current && clear.current();
+    };
+  }, [callback, delay]);
+
+  return () => {
+    clear.current && clear.current();
+  };
 };
 
 export default useTimeout;
