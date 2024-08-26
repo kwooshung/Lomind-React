d9c569e98edadbb22ada4f2e8e1c05b0cabb8f1a
kwooshung <kwooshung@qq.com>
2024-08-26 20:03:27 +0800

    🐛 fix(components): The new version, fixed some hook implementation

--- src/useOS/index.ts
+++ src/useOS/index.ts
@@ -6,7 +6,7 @@ import { IOSInfo, TOSResult } from './interfaces';
 /**
  * @zh 只需要系统信息，可以使用此 Hook；若一个组件中包含 useBrowser、useOs、useIsMobile、useIsTablet，其中任意两个及以上，建议使用 useDetector
  * @en If you only need system information, you can use this Hook; if a component contains useBrowser, useOs, useIsMobile, useIsTablet, any two or more, it is recommended to use useDetector
- * @returns {TOSResult} 系统返回值
+ * @returns {TOSResult} 系统信息返回值
  */
 const useOS = (): TOSResult => {
   const [osInfo, setOsInfo] = useState<IOSInfo>();
