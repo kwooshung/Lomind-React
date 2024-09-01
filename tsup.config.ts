f895400c21bc4aa187fb4037f9a71f46604c49fc
kwooshung <kwooshung@qq.com>
2024-09-02 07:25:38 +0800

    🐛 fix(config): Fix Package.json to better support CJS

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,13 @@
 export default {
   clean: true,
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  dts: true,
+  entry: ['src/index.ts'], // 入口文件
+  format: ['cjs', 'esm'], // 生成的格式
+  dts: {
+    resolve: true
+  }, // 生成类型定义文件
+  minify: true, // 是否压缩
+  sourcemap: false, // 是否生成 sourcemap
   outExtension: ({ format }) => ({
-    js: format === 'cjs' ? '.cmd.js' : format === 'esm' ? '.esm.js' : 'umd.js'
+    js: format === 'esm' ? '.mjs' : '.cjs'
   })
 };
